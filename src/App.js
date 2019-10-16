f65c7cde69eca89207401d1a51851cc754070aa5
George Mauer <gmauer@gmail.com>
2019-10-15 23:19:37 -0500

    the entire basic entry form is working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useState, useEffect} from 'react'
+import styled from 'styled-components'
 import './App.css';
 import {getFoods} from './foods.js'
+
+const Input = styled.input`
+    &[required]:invalid {
+        background-color: ##ffd0d0;
+    }
+`
 
 const ApiContext = React.createContext({
     getFoods
@@ -48,19 +55,80 @@ const FoodSelector = ({values, onChange}) => {
 }
 
 const noop = () => {}
-const MealItemEntry = ({meal, onChange}) => (
-    <form onSubmit={noop}>
-        <header><ShortMealDisplay {...meal.food} /></header>
-      <input placeholder="Portions Eaten" type="number" min={1} value={meal.portionsEaten} onChange={onChange} />
-    </form>
-)
+const MealItemEntry = ({meal, onChange}) => {
+    const changePortions = ({target: {value}}) => {
+        onChange({...meal, portionsEaten: +value})
+    }
+    return (
+        <form onSubmit={noop}>
+          <header>
+            <ShortMealDisplay {...meal.food} />
+          </header>
+          <Input placeholder="Portions Eaten"
+                 type="number" min={1} required
+                 value={meal.portionsEaten}
+                 onChange={changePortions} />
+        </form>
+    )
+}
 
+const match = (items, others, areEqual) => {
+    const additional = [...others]
+    const removed = []
+    for(const i of items) {
+        const idx = additional.findIndex(areEqual(i))
+        if(idx < 0)
+            removed.push(i)
+        else
+            additional.splice(idx, 1)
+    }
+    return {additional, removed}
+}
+const range = function * (min = 0) {
+    let val = min;
+    while(true) {
+        yield val
+        val += 1
+    }
+}
+const zip = function * (coll1, coll2) {
+    const it2 = coll2[Symbol.iterator]()
+    for(const item1 of coll1) {
+        const {done, value} = it2.next()
+        if(done)
+            return
+        yield [item1, value]
+    }
+}
+const replace = ({act, where}) => function * (arr) {
+    for(const [x, i] of zip(arr, range()))
+        if(where(x, i))
+            yield act(x, i)
+        else
+            yield x
+}
+const arr = x => Array.isArray(x) ? x : [...x]
+const createMeal = food => ({
+    food,
+    portionsEaten: 1,
+})
 const MealBuilder = () => {
     const [mealItems, setMealItems] = useState([])
     const onMealItemsChange = (foods) => {
-        setMealItems(foods.map(food => ({food})))
+        const {additional, removed} = match(mealItems, foods, mi => f => mi.food.id === f.id)
+        const newFoods = mealItems
+              .filter(mi => !removed.includes(mi))
+              .concat(additional.map(createMeal))
+        setMealItems(newFoods)
     }
-    const onMealEntryChange = (x) => console.log(x)
+    const onMealEntryChange = (x) => {
+        setMealItems(arr(
+            replace({
+                act: o => ({...o, portionsEaten: x.portionsEaten}),
+                where: (o) => o.food.id === x.food.id
+            })(mealItems)
+        ))
+    }
     return (
         <section>
           <header>Build your meal</header>
